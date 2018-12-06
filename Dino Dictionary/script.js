35a853c8234db16f9f0f634bb64b73ef15eb61e0
frankmarcon <43049091+frankmarcon@users.noreply.github.com>
2018-12-06 18:02:56 -0500

    Add files via upload

---
+++ Dino Dictionary/script.js
@@ -0,0 +1,142 @@
+$(document).ready(function() {
+  $(".carnobox").hover(function() {
+  	$(".carnoinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+
+
+$(document).ready(function() {
+  $(".box6").hover(function() {
+  	$(".tinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".parabox").hover(function() {
+  	$(".parainfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".gallbox").hover(function() {
+  	$(".gallinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  	 });
+  });
+
+$(document).ready(function() {
+  $(".box4").hover(function() {
+  	$(".triinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".box5").hover(function() {
+  	$(".ptinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb1").hover(function() {
+  	$(".compinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb2").hover(function() {
+  	$(".herinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb3").hover(function() {
+  	$(".melaninfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb4").hover(function() {
+  	$(".platinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb5").hover(function() {
+  	$(".stakinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".tb6").hover(function() {
+  	$(".saltinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb1").hover(function() {
+  	$(".bracinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb2").hover(function() {
+  	$(".archinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb3").hover(function() {
+  	$(".diloinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb4").hover(function() {
+  	$(".alloinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb5").hover(function() {
+  	$(".steginfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+$(document).ready(function() {
+  $(".jb6").hover(function() {
+  	$(".dipinfo").toggleClass("show");
+  	$(".b2").toggleClass("show")
+  });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
