5c6284c0912f414777ca3052f02c7191aa8367ca
frankmarcon <43049091+frankmarcon@users.noreply.github.com>
2020-02-27 15:23:54 -0500

    Add files via upload

--- Racers/script.js
+++ Racers/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
   
   setTimeout(function() {
+    $(".purple").addClass("alarm");
+  }, 1000);
+
+
+	setTimeout(function() {
     $(".orange").addClass("alarm");
+  }, 1000);
+
+	setTimeout(function() {
+    $(".explode").addClass("alarm");
   }, 2000);
+
+	setTimeout(function() {
+    $(".fire").addClass("alarm");
+  }, 2500);
 });
